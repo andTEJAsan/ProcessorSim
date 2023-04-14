9057f24c88fc4457ac8828e29aac7a434ae43ff1
Aavegjain <97734426+Aavegjain@users.noreply.github.com>
2023-04-14 13:44:53 +0530

    part 5

---
+++ BranchPredictor.cpp
@@ -0,0 +1,52 @@
+#include "BranchPredictor.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+void tester(int x) {
+
+    std::ifstream file("branchtrace.txt"); // open the file
+    std::string line;
+    struct SaturatingBranchPredictor sbp(x);
+    struct BHRBranchPredictor bbp(x);
+    struct SaturatingBHRBranchPredictor sbbp(x,1<<16);
+    int sbp_count=0;
+    int bbp_count =0;
+    int sbbp_count=0;
+    int count=0;
+    while (std::getline(file, line)) { // read the file line by line
+        //std::cout << line[9]<<std::endl; // do something with each line
+        uint32_t pc =static_cast<uint32_t>( std::stoul(line.substr(0,8),nullptr,16));
+        bool taken;
+        //std::cout <<std::bitset<32>(pc) << "\n";
+        if (line[9]=='1') { taken = true; }
+        else {taken =false;}
+        if (taken == sbp.predict(pc)) {sbp_count++; }
+        sbp.update(pc,taken);
+        if (taken == bbp.predict(pc)) {bbp_count++; }
+        bbp.update(pc,taken);
+        if (taken == sbbp.predict(pc)) {sbbp_count++; }
+        sbbp.update(pc,taken);
+        count++;
+
+
+    }
+    float sbp_accuracy =(float)(sbp_count) / count;
+    float bbp_accuracy = (float)(bbp_count) / count;
+    float sbbp_accuracy = (float)(sbbp_count) / count;
+    cout << "Initialization :- " << x << "\n";
+    cout << "SBP Accuracy = " << sbp_accuracy << "\n";
+    cout << "BBP Accuracy = " << bbp_accuracy << "\n";
+    cout << "SBBP Accuracy = " << sbbp_accuracy << "\n";
+    cout << "---------------------------------"<<"\n";
+    file.close(); // close the file
+ 
+}
+int main() {
+   tester(0);
+   tester(1);
+   tester(2);
+   tester(3);
+   return 0;
+}
---
+++ BranchPredictor.hpp
@@ -0,0 +1,197 @@
+#ifndef __BRANCH_PREDICTOR_HPP__
+#define __BRANCH_PREDICTOR_HPP__
+
+#include <vector>
+#include <bitset>
+#include <cassert>
+struct BranchPredictor {
+    virtual bool predict(uint32_t pc) = 0;
+    virtual void update(uint32_t pc, bool taken) = 0;
+};
+
+struct SaturatingBranchPredictor : public BranchPredictor {
+    std::vector<std::bitset<2>> table;
+    SaturatingBranchPredictor(int value) : table(1 << 14, value) {}
+    
+    bool predict(uint32_t pc) {
+        // your code here
+        std::bitset<2> x = table[pc % (1 << 14)];
+        return x.test(1); // the leftmost bit is true
+    }
+
+    void update(uint32_t pc, bool taken) {
+        // your code here
+        if (taken)
+        {
+            std::bitset<2> x = table[pc % (1<<14)];
+            int sum = 2*((int) x.test(1)) + (int) x.test(0);
+            int new_sum = sum +1;
+            if (new_sum == 4) new_sum = 3;
+            x.set(0,(bool) (new_sum %2));
+            x.set(1,(bool)(new_sum & 2));
+            table[pc %(1<<14)] = x;
+        return;
+        }
+        std::bitset<2> x = table[pc % (1<<14)];
+        int new_sum = 0;
+        int sum = 2*((int) x.test(1)) + (int) x.test(0);
+        if ( sum == 0 ) new_sum = 0;
+        else new_sum = sum - 1;
+        x.set(0,(bool) (new_sum %2));
+        x.set(1,(bool)(new_sum & 2));
+        table[pc %(1<<14)] = x;       
+    }
+};
+
+struct BHRBranchPredictor : public BranchPredictor {
+    std::vector<std::bitset<2>> bhrTable;
+    std::bitset<2> bhr;
+    BHRBranchPredictor(int value) : bhrTable(1 << 2, value),bhr(value) {}
+    bool predict(uint32_t pc) {
+        // your code here
+    int address = static_cast<int> (bhr.to_ulong()); 
+    return (bhrTable[address]).test(1);
+    }
+
+    void update(uint32_t pc, bool taken) {
+        // your code here
+        //std::bitset<2> y;
+        //y.set(1,taken);
+        //y.set(0,bhr.test(1));
+        //bhr= y; 
+        if (taken) 
+        {
+            int address = static_cast<int> (bhr.to_ulong()); 
+            std::bitset<2> counter = bhrTable[address];
+            int sum = static_cast<int> (bhr.to_ulong());
+            int new_sum = sum +1;
+            if (new_sum == 4) new_sum = 3;
+            counter.set(0,(bool) (new_sum %2));
+            counter.set(1,(bool)(new_sum & 2));
+            bhrTable[address]=counter;
+        }
+        else {
+            int address = static_cast<int> (bhr.to_ulong()); 
+            std::bitset<2> counter = bhrTable[address];
+            int sum = static_cast<int> (counter.to_ulong());
+            int new_sum;
+            if (sum == 0) new_sum = 0;
+            else new_sum = sum - 1;
+            counter.set(0,(bool) (new_sum %2));
+            counter.set(1,(bool)(new_sum & 2));
+            bhrTable[address]=counter;
+        }
+        std::bitset<2> y;
+        y.set(1, taken);
+        y.set(0, bhr.test(1));
+        bhr = y;
+    } 
+};
+
+struct SaturatingBHRBranchPredictor : public BranchPredictor {
+    std::vector<std::bitset<2>> bhrTable;
+    std::bitset<2> bhr;
+    std::vector<std::bitset<2>> table;
+    std::vector<std::bitset<2>> combination;
+    SaturatingBHRBranchPredictor(int value, int size) : bhrTable(1 << 2, value),bhr(value), table(1 << 14, value), combination(size, value) {
+    assert(size <= (1 << 16));
+    }
+    
+    // size == 1 << 16;
+    bool predict(uint32_t pc) {
+        int size = combination.size();
+        // your code here
+        // if predicted strongly not taken or strongly taken by the local branch predictor then follow that 
+        // otherwise look at the global branch predictor if it is strongly taken or not taken then follow it else go to combination table
+        std::bitset<2> locpredict = table[pc %(1<<14)];
+        int loc_predict = static_cast<int> ((locpredict).to_ulong());
+        if (loc_predict==3 || loc_predict==0) return locpredict.test(1);
+        int address = static_cast<int> (bhr.to_ulong()); 
+        std::bitset<2> bhrpredict = (bhrTable[address]);
+        int bhrP = static_cast<int> (bhrpredict.to_ulong());
+        if (bhrP==3 || bhrP==0) return bhrpredict.test(1);
+        int index_combination = (4*(pc %(1<<14)) + address) % size ; 
+        std::bitset<2> combr = combination[index_combination];
+        return combr.test(1);
+    }
+
+
+    void update(uint32_t pc, bool taken) {
+        // your code here
+        int size  = combination.size();
+        if (taken){
+            std::bitset<2> x = table[pc % (1<<14)];
+            int sum = 2*((int) x.test(1)) + (int) x.test(0);
+            int new_sum = sum +1;
+            if (new_sum == 4) new_sum = 3;
+            x.set(0,(bool) (new_sum %2));
+            x.set(1,(bool)(new_sum & 2));
+            table[pc %(1<<14)] = x;
+
+            int address = static_cast<int> (bhr.to_ulong()); 
+            std::bitset<2> counter = bhrTable[address];
+            sum = static_cast<int> (bhr.to_ulong());
+            new_sum = sum +1;
+            if (new_sum == 4) new_sum = 3;
+            counter.set(0,(bool) (new_sum %2));
+            counter.set(1,(bool)(new_sum & 2));
+            bhrTable[address]=counter;
+
+
+            int index_combination = (4*(pc %(1<<14)) + address) % size ; 
+             x = combination[index_combination];
+            sum = 2*((int) x.test(1)) + (int) x.test(0);
+            new_sum = sum +1;
+            if (new_sum == 4) new_sum = 3;
+            x.set(0,(bool) (new_sum %2));
+            x.set(1,(bool)(new_sum & 2));
+            combination[index_combination] = x;
+
+
+        }
+        else {
+        std::bitset<2> x = table[pc % (1<<14)];
+        int new_sum = 0;
+        int sum = 2*((int) x.test(1)) + (int) x.test(0);
+        if ( sum == 0 ) new_sum = 0;
+        else new_sum = sum - 1;
+        x.set(0,(bool) (new_sum %2));
+        x.set(1,(bool)(new_sum & 2));
+        table[pc %(1<<14)] = x;
+
+
+        int address = static_cast<int> (bhr.to_ulong()); 
+            std::bitset<2> counter = bhrTable[address];
+            sum = static_cast<int> (counter.to_ulong());
+            if (sum == 0) new_sum = 0;
+            else new_sum = sum - 1;
+            counter.set(0,(bool) (new_sum %2));
+            counter.set(1,(bool)(new_sum & 2));
+            bhrTable[address]=counter;
+
+
+        int index_combination = (4*(pc %(1<<14)) + address) % size ; 
+        x = combination[index_combination];
+        sum = 2*((int) x.test(1)) + (int) x.test(0);
+        if ( sum == 0 ) new_sum = 0;
+        else new_sum = sum - 1;
+        x.set(0,(bool) (new_sum %2));
+        x.set(1,(bool)(new_sum & 2));
+        combination[index_combination] = x;
+
+
+
+        }
+    
+    std::bitset<2> y;
+    y.set(1, taken);
+    y.set(0, bhr.test(1));
+    bhr = y;
+   
+    
+    
+    }
+
+};
+
+#endif
